28c1a7ea878e54c41d94e218a4bd3f7f0b8d4275
hcchong <chong@azbizsetup.com>
2019-07-28 22:56:34 +0800

    added analytics and livechat

--- application/views/web/templates/footer.php
+++ application/views/web/templates/footer.php
@@ -36,6 +36,34 @@
     <script src="<?= base_url() ?>assets/web/js/bootstrap.min.js"></script>
     <script src="<?= base_url() ?>js/LLC.js"></script>
     <script src="<?= base_url() ?>assets/web/js/script.js"></script>
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://sohotrack.com/analytics/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d3b1b429b94cd38bbe97aa8/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
